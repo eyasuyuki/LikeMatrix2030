fc2dcc71686b7aec0063a1292bb4386824b09bbf
Yasuyuki ENDO <eyasuyuki@gmail.com>
2026-03-20 05:02:45 +0900

    initial commit

---
+++ qmk_firmware/keyboards/likematrix2030/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x88, 0x02, 0x9b, 0x75, 0x54, 0x35, 0xc3, 0x74}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
